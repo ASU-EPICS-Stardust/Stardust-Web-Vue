b7c61bb3261b7a8a2465682788ae5315a39481df
Aaron Kampmeier <aaron.kampmeier@gmail.com>
2020-10-19 16:54:45 -0700

    Added in the vue-cli-plugin-s3 deploy and created a script to deploy the dist site up to our public S3 bucket. Now the site is available publicly at https://stardust-pv-website.s3.us-east-2.amazonaws.com/index.html.

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,27 @@
 //vue.config.js
 module.exports = {
 	pluginOptions: {
-		apollo: {
-			lintGQL: 'true'
-		}
-	}
+      apollo: {
+        lintGQL: 'true'
+      },
+      s3Deploy: {
+        registry: undefined,
+        awsProfile: 'default',
+        overrideEndpoint: false,
+        region: 'us-east-2',
+        bucket: 'stardust-pv-website',
+        createBucket: false,
+        staticHosting: true,
+        staticIndexPage: 'index.html',
+        staticErrorPage: 'index.html',
+        assetPath: 'dist',
+        assetMatch: '**',
+        deployPath: '/',
+        acl: 'public-read',
+        pwa: false,
+        enableCloudfront: false,
+        pluginVersion: '4.0.0-rc3',
+        uploadConcurrency: 5
+      }
+    }
 }
