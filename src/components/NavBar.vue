38e8a7333e0da86b86171c0657a8299112f8514d
Tommy Blunt <tommy@historicagency.com>
2020-10-24 17:13:06 -0700

    Removed Vuetify branding from navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+			src="../assets/solar-panel-solid.svg"
+			width=40
+			class = "mr-4"
+        > </v-img>
+		<v-toolbar-title>
+			Stardust Panel Testing
+		</v-toolbar-title>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-app-bar-nav-icon>
+      </v-app-bar-nav-icon>
+    </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
